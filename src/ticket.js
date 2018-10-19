55ce4b8f966c4e0de3b25c2c27f54e7acd9a53bb
Kevin Cromwell <kevincromwell@Andrews-MacBook-Air.local>
2018-10-18 20:13:24 -0500

    Added a Login Class, and error handling for API authentication

--- src/ticket.js
+++ src/ticket.js
@@ -1,22 +1,64 @@
 import React, { Component } from 'react';
 
+class TicketLogin extends React.Component {
+  constructor(){
+    super();
+    this.state = {  };
+  }
+
+  render() {
+    return (
+      <div>
+    <p>
+      <label for='Username'>User Name:</label>
+      <input type='text' name='Username' id='Username' />
+   </p>
+   <p>
+    <label for='Password'>Password:</label>
+      <input type='text' name='Password' id='Password' />
+</p>
+    </div>
+    );
+  }
+}
+
 class TicketView extends React.Component {
 
   constructor(){
     super();
     this.state = {
       tickets : [],
+      isLoading: false,
+      error: null,
     };
   }
 
   componentDidMount(){
+    this.setState({ isLoading: true });
     fetch('http://127.0.0.1:8000/api:bar/')
-      .then(resp => resp.json())
-      .then(data => this.setState({ tickets: data.results }));
+      .then(resp => {
+        if(resp.ok) {
+          return resp.json();
+        } else if(resp.status === 401) {
+          throw new Error('Need to Login ...');
+        } else {
+          throw new Error('Something went wrong ...');
+        }
+      })
+      .then(data => this.setState({ tickets: data.results, isLoading: false }))
+      .catch(error => this.setState({ error, isLoading: false }));
   }
 
   render(){
-    const {tickets} = this.state;
+    const {tickets,isLoading,error} = this.state;
+    if( error ) {
+      return <p>{error.message}
+        { new TicketLogin().render() }
+            </p>;
+    }
+    if( isLoading ) {
+      return <p>Loading ...</p>;
+    }
     return (
       <ul>
         {tickets.map(ticket =>
